52c42c881c9a63c9155436913618ef1b50f26fd5
lWWl <825454474@qq.com>
2019-04-04 15:46:23 +0800

    Add files via upload
    非递归实现

---
+++ BinaryTree/binary-tree-traversal.cpp
@@ -0,0 +1,121 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+		if (!root)
+			return result;
+
+		stack<TreeNode*> s;
+		auto tmp = root;
+	
+		while (!s.empty() || tmp)
+		{
+			while (tmp->left)
+			{
+				s.push(tmp->left);
+				tmp = tmp->left;
+			}
+
+			tmp = s.top();
+			result.push_back(tmp->val);
+			s.pop();
+			tmp = tmp->right;
+		}
+		
+		return result;
+    }
+};
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (root == nullptr)
+			return {};
+		
+		stack<TreeNode*> st;
+		vector<int> result;
+		TreeNode* tmp = root, *last = root;
+		st.push(tmp);
+		
+		while (!st.empty())
+		{
+			tmp = st.top();
+			if ((tmp->left == nullptr && tmp->right == nullptr) || (tmp->right == nullptr && tmp->left == last) || tmp->right == last)
+			{				
+                result.push_back(tmp->val);
+				st.pop();
+				last = tmp; 
+			}
+            else 
+            {
+			    if (tmp->right)
+                {
+                    st.push(tmp->right);
+                }
+			    if (tmp->left)
+                {
+			    	st.push(tmp->left);
+                }
+            }
+		}
+		
+		return result;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+		if (root == nullptr)
+			return vector<int>{};
+		
+		stack<TreeNode*> st;
+		vector<int> result;
+		st.push(root);
+		
+		while (!st.empty())
+		{
+			TreeNode* tmp = st.top();
+			result.push_back(tmp->val);
+			st.pop();
+			
+			if (tmp->right)
+				st.push(tmp->right);
+			if (tmp->left)
+				st.push(tmp->left);
+		}
+		
+		return result;
+    }
+};
