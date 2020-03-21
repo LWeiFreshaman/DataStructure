b7243649c6ec3666bd6f53bf68fae51049cd74a7
lWWl <825454474@qq.com>
2020-03-21 11:00:30 +0800

    Update sort_func.h

--- sort/sort_func.h
+++ sort/sort_func.h
@@ -52,4 +52,56 @@ void quick_sort(T arr[], int n)
 	qsort(arr, 0, n - 1);
 }
 
+template <typename Comparable>
+void bubble_sort(vector<Comparable>& vec)
+{
+    int flag = 0;
+    for (int i = vec.size() - 1; i >0; --i)
+    {
+        flag = 0;
+        for (int j = 0; j < i; ++j)
+        {
+            if (vec[j] > vec[j + 1])
+            {
+                swap(vec[j], vec[j + 1]);
+                flag = 1;
+            }
+        }
+
+        if (!flag)
+            break;
+    }
+}
+
+template <typename Comparable>
+void insertion_sort(vector<Comparable>& vec)
+{
+    for (int i = 1; i < vec.size(); ++i)
+    {
+        int tmp = vec[i];
+        int j;
+        for (j = i; j > 0 && tmp < vec[j - 1]; --j)
+        {
+            vec[j] = vec[j - 1];
+        }
+        vec[j] = tmp;
+    }
+}
+
+template <typename Comparable>
+void shell_sort(vector<Comparable>& vec)
+{
+    for (int i = vec.size() / 2; i > 0; i /= 2)
+    {
+        for (int j = i; j < vec.size(); ++j)
+        {
+            int tmp = vec[j];
+            int k;
+            for (k = j; k > 0 && tmp < vec[k - i]; k -= i)
+                vec[k] = vec[k - i];
+            vec[k] = tmp;
+        }
+    }
+}
+
 #endif 
